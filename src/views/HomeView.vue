c6528ce99226ea6174e0354b1939e687cdfbaf41
chewryl <cherylmataitini@gmail.com>
2023-11-18 23:09:17 +0000

    initial commit

---
+++ src/views/HomeView.vue
@@ -0,0 +1,21 @@
+<template>
+	<div class="home container">
+		<p>home</p>
+	</div>
+</template>
+
+<script>
+export default {
+	setup () {
+		return {}
+	}
+}
+</script>
+
+<style lang="scss">
+.home {
+	text-align: center;
+	padding: 20px 0;
+}
+
+</style>
