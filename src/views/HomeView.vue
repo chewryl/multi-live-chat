5deca70987a5e54b5562055ea5c28ec45126bbe4
chewryl <cherylmataitini@gmail.com>
2023-11-19 20:14:52 +0000

    add login and signup form

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,11 +1,20 @@
 <template>
 	<div class="home container">
 		<p>home</p>
+		<SignupForm />
+		<!-- <LoginForm /> -->
 	</div>
 </template>
 
 <script>
+import SignupForm from '@/components/SignupForm.vue';
+import LoginForm from '@/components/LoginForm.vue';
+
 export default {
+	components: {
+		SignupForm,
+		LoginForm
+	},
 	setup () {
 		return {}
 	}
@@ -16,6 +25,22 @@ export default {
 .home {
 	text-align: center;
 	padding: 20px 0;
+	form {
+		width: 300px;
+		margin: 20px auto;
+	}
+	label {
+		display: block;
+		margin: 20px 0 10px;
+	}
+	input {
+		width: 100%;
+		padding: 10px;
+		border-radius: 3px;
+		border: 1px solid #eee;
+		outline: none;
+		color: #999;
+		margin: 10px auto;
+	}
 }
-
 </style>
