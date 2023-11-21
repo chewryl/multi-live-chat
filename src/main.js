4395f6b8d1b9a0a1b1ac87df933ecaa1cd70fa2d
chewryl <cherylmataitini@gmail.com>
2023-11-21 22:33:27 +0000

    fix: redirecting to login page if authenticated user refreshes chatroom. Wait for firebase connection

--- src/main.js
+++ src/main.js
@@ -2,5 +2,17 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import '@/assets/main.scss'
+import { projectAuth } from './firebase/config'
 
-createApp(App).use(router).mount('#app')
+let app
+
+projectAuth.onAuthStateChanged(() => {
+	if (!app) {
+		// wait for initial user value from firebase before rendering the app
+		// firebase connection is now established
+		app = createApp(App)
+			.use(router)
+			.mount('#app')
+	}
+})
+
