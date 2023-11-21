1fad1eaeecae6862b52802cfe11caa1cfc26f376
chewryl <cherylmataitini@gmail.com>
2023-11-21 22:48:40 +0000

    watch current user and redirect away from chatroom if they logout

--- src/views/ChatRoomView.vue
+++ src/views/ChatRoomView.vue
@@ -6,9 +6,24 @@
 </template>
 <script>
 import Navbar from '@/components/Navbar.vue';
+import getUser from '@/composables/getUser';
+import { watch } from 'vue';
+import { useRouter } from 'vue-router';
+
 export default {
 	components: {
 		Navbar
+	},
+	setup () {
+		const { user } = getUser()
+		const router = useRouter()
+
+		watch(user, newState => {
+			// user has logged out
+			if (!newState) {
+				router.push({ name: 'home' })
+			}
+		})
 	}
 }
 </script>
