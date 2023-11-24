f33d51c53917ee0f58a45a25240dec8e2203a3ce
chewryl <cherylmataitini@gmail.com>
2023-11-24 01:32:14 +0000

    wip: different color username for each user in the chat

--- src/components/ChatWindow.vue
+++ src/components/ChatWindow.vue
@@ -2,9 +2,9 @@
 	<div class="chat-window">
 		<div v-if="error">{{ error }}</div>
 		<div v-if="docs" class="messages">
-			<div v-for="doc in docs" :key="doc.id" class="single-message">
+			<div v-for="doc in messagesWithUserColor" :key="doc.id" class="single-message">
 				<span class="created-at">{{ doc.createdAt.toDate() }}</span>
-				<span class="user">{{ doc.user }}</span>
+				<span class="user" :style="{'color': doc.color}">{{ doc.user }}</span>
 				<span class="message">{{ doc.message }}</span>
 			</div>
 		</div>
@@ -13,14 +13,36 @@
 
 <script>
 import getCollection from '@/composables/getCollection';
+import { computed } from 'vue';
 
 export default {
 	setup () {
 		const { docs, error } = getCollection('messages')
+		let usernameColor = []
+
+		const messagesWithUserColor = computed(() => {
+			docs.value.forEach(doc => {
+				if (!usernameColor.includes(doc.user)) {
+					usernameColor.push({ user: doc.user, color: generateRandomColor() })
+				}
+			})
+			let messagesWithUserColor = docs.value.map(doc => {
+				let color = usernameColor.find(u => u.user === doc.user).color
+				return { ...doc, color }
+			})
+			return messagesWithUserColor.sort((x, y) => {
+				return x.createdAt - y.createdAt
+			})
+		})
+
+		const generateRandomColor = () => {
+			return "#" + ((1 << 24) * Math.random() | 0).toString(16).padStart(6, "0")
+		}
 
 		return {
 			docs,
-			error
+			error,
+			messagesWithUserColor
 		}
 	}
 }
